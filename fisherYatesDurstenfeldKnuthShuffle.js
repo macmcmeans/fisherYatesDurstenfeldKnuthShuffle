55a28efbf03f7fe744d871df6e2a7178b3c0271b
W. "Mac" McMeans <28242167+macmcmeans@users.noreply.github.com>
2020-05-03 23:19:41 -0400

    Update fisherYatesDurstenfeldKnuthShuffle.js

--- fisherYatesDurstenfeldKnuthShuffle.js
+++ fisherYatesDurstenfeldKnuthShuffle.js
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------
-Copyright (c) 2018, William McMeans
+Copyright (c) 2018, W. "Mac" McMeans
 BSD License.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
